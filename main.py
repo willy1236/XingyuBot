64fcf28357767adc0f46c0d17a4a057f1e395cef
立 <66502303+willy1236@users.noreply.github.com>
2022-03-20 06:43:57 +0000

    20220320-r1

--- main.py
+++ main.py
@@ -221,7 +221,7 @@ async def permission(ctx,guild_id:int):
 #         else:
 #             await channel.send('👍')
 
-ignore_py = []
+ignore_py = ['music']
 for filename in os.listdir('./cmds'):
     if filename.endswith('.py') and filename[:-3] not in ignore_py:
         bot.load_extension(f'cmds.{filename[:-3]}')
