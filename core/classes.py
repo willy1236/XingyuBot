aa9b4a41725c203d32b48bd1ffbb7cc09a03ff96
willy1236 <willyjiang607@gmail.com>
2022-12-29 23:38:41 +0800

    Update classes.py

--- core/classes.py
+++ core/classes.py
@@ -1,11 +1,17 @@
 from discord.ext import commands
 from BotLib.database import MySQLDatabase,JsonDatabase
 
-try:
-    SQLsettings = JsonDatabase().jdata["SQLsettings"]
-    sqldb = MySQLDatabase(**SQLsettings)
-    print('>> SQL connect: on <<')
-except:
+jdata = JsonDatabase().jdata
+SQL_connection = jdata.get('SQL_connection')
+
+if SQL_connection:
+    try:    
+        SQLsettings = jdata["SQLsettings"]
+        sqldb = MySQLDatabase(**SQLsettings)
+        print('>> SQL connect: on <<')
+    except:
+        print('>> SQL connect: offline <<')
+else:
     print('>> SQL connect: off <<')
 
 class Cog_Extension(commands.Cog):
