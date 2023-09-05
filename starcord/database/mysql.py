8d918da2dc5efece144f90b7ff281a3e59fb6863
willy1236 <willyjiang607@gmail.com>
2023-09-05 17:04:47 +0800

    Update mysql.py

--- starcord/database/mysql.py
+++ starcord/database/mysql.py
@@ -78,7 +78,7 @@ class MySQLDatabase():
         self.connection.commit()
         
     def add_userdata_value(self,user_id:int,table:str,column:str,value):
-        self.cursor.execute(f"INSERT INTO `{table}` SET user_id = {user_id}, {column} = {value} ON DUPLICATE KEY UPDATE `{column}` = `{column}` + {value};")
+        self.cursor.execute(f"INSERT INTO `{table}` SET user_id = {user_id}, {column} = {value} ON DUPLICATE KEY UPDATE `user_id` = {user_id}, `{column}` = `{column}` + {value};")
         self.connection.commit()
 
     # 遊戲資料類
