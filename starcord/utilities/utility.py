13ad16b63018f66d54cefe49e66bf372d704cb50
willy1236 <willyjiang607@gmail.com>
2024-03-04 19:04:44 +0800

    配合py-cord 2.5.0

--- starcord/utilities/utility.py
+++ starcord/utilities/utility.py
@@ -6,36 +6,36 @@ from .funtions import find
 
 class BotEmbed:
     @staticmethod
-    def bot(bot:discord.Bot,title:str=discord.Embed.Empty,description:str=discord.Embed.Empty,url=discord.Embed.Empty):
+    def bot(bot:discord.Bot,title:str=None,description:str=None,url=None):
         '''機器人 格式'''
         embed = discord.Embed(title=title,description=description, color=0xc4e9ff,url=url)
         embed.set_author(name=bot.user.name,icon_url=bot.user.display_avatar.url)
         return embed
     
     @staticmethod
-    def user(user:discord.User,title:str=discord.Embed.Empty,description:str=discord.Embed.Empty,url=discord.Embed.Empty):
+    def user(user:discord.User,title:str=None,description:str=None,url=None):
         '''使用者 格式'''
         embed = discord.Embed(title=title,description=description, color=0x00ffff,url=url)
         embed.set_author(name=user.name,icon_url=user.display_avatar.url)
         return embed
     
     @staticmethod
-    def simple(title:str=discord.Embed.Empty,description:str=discord.Embed.Empty,url=discord.Embed.Empty):
+    def simple(title:str=None,description:str=None,url=None):
         '''簡易:不帶作者'''
-        embed = discord.Embed(title=title,description=description or discord.Embed.Empty, color=0xc4e9ff,url=url)
+        embed = discord.Embed(title=title,description=description or None, color=0xc4e9ff,url=url)
         return embed
 
     @staticmethod
-    def general(name:str=discord.Embed.Empty,icon_url:str=discord.Embed.Empty,url:str=discord.Embed.Empty,title:str=discord.Embed.Empty,description:str=discord.Embed.Empty):
+    def general(name:str=None,icon_url:str=None,url:str=None,title:str=None,description:str=None):
         '''普通:自訂作者'''
-        embed = discord.Embed(title=title,description=description or discord.Embed.Empty,color=0xc4e9ff)
+        embed = discord.Embed(title=title,description=description or None,color=0xc4e9ff)
         embed.set_author(name=name,icon_url=icon_url,url=url)
         return embed
     
     @staticmethod
-    def rpg(title:str=discord.Embed.Empty,description:str=discord.Embed.Empty):
+    def rpg(title:str=None,description:str=None):
         """RPG系統 格式"""
-        embed = discord.Embed(title=title,description=description or discord.Embed.Empty, color=0xc4e9ff)
+        embed = discord.Embed(title=title,description=description or None, color=0xc4e9ff)
         embed.set_footer(text= "RPG系統（開發版） | 開發時期所有東西皆有可能重置")
         return embed
 
@@ -72,9 +72,9 @@ class BotEmbed:
         return embed
     
     @staticmethod
-    def info(title:str=discord.Embed.Empty,description:str=discord.Embed.Empty,url:str=discord.Embed.Empty):
+    def info(title:str=None,description:str=None,url:str=None):
         '''一般資訊 格式'''
-        embed = discord.Embed(title=title,description=description or discord.Embed.Empty, color=0xc4e9ff,url=url)
+        embed = discord.Embed(title=title,description=description or None, color=0xc4e9ff,url=url)
         return embed
 
 
