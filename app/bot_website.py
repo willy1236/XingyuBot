68da047d5d80e88276f701c7f6b8e40eaa944a78
willy1236 <66502303+willy1236@users.noreply.github.com>
2024-10-04 20:34:06 +0800

    Add: /to and change oauth route.

--- app/bot_website.py
+++ app/bot_website.py
@@ -1,33 +1,57 @@
 import asyncio
+import secrets
 from datetime import datetime, timedelta
 
 import feedparser
-from fastapi import BackgroundTasks, FastAPI, HTTPException
+from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException, status
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.openapi.utils import get_openapi
 from fastapi.requests import Request
 from fastapi.responses import (HTMLResponse, JSONResponse, PlainTextResponse,
-                               StreamingResponse)
+                               RedirectResponse, StreamingResponse)
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from linebot.v3 import WebhookHandler
 from linebot.v3.exceptions import InvalidSignatureError
 from linebot.v3.messaging import (ApiClient, Configuration, MessagingApi,
                                   ReplyMessageRequest, TextMessage)
 from linebot.v3.webhooks import MessageEvent, TextMessageContent
 from linebot.v3.webhooks.models.message_event import MessageEvent
+from tunnel_threads import BaseThread
 
 from starlib import Jsondb, sclient, web_log
 from starlib.dataExtractor import DiscordOauth, TwitchOauth
 from starlib.models.mysql import CloudUser, TwitchBotJoinChannel
 from starlib.models.push import YoutubePush
 
-from .tunnel_threads import BaseThread
-
-app = FastAPI()
-
 discord_oauth_settings = Jsondb.get_token("discord_oauth")
 twitch_oauth_settings = Jsondb.get_token("twitch_chatbot")
 linebot_token = Jsondb.get_token("line_bot")
+docs_account = Jsondb.get_token("docs_account")
 
 configuration = Configuration(access_token=linebot_token.get("token"))
 handler = WebhookHandler(linebot_token.get("secret"))
+
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+
+security = HTTPBasic()
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)):
+    correct_username = secrets.compare_digest(credentials.username, docs_account["account"])
+    correct_password = secrets.compare_digest(credentials.password, docs_account["password"])
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/docs", include_in_schema=False)
+async def get_documentation(username: str = Depends(get_current_username)):
+    return get_swagger_ui_html(openapi_url="/openapi.json", title="docs")
+
+@app.get("/openapi.json", include_in_schema=False)
+async def openapi(username: str = Depends(get_current_username)):
+    return get_openapi(title="FastAPI", version="0.1.0", routes=app.routes)
 
 @app.route('/')
 def main(request:Request):
@@ -88,8 +112,8 @@ async def youtube_push_post(request:Request,background_task: BackgroundTasks):
     background_task.add_task(get_yt_push,body)
     return HTMLResponse('OK')
 
-@app.route('/discord',methods=['GET'])
-async def discord_oauth(request:Request):
+@app.get('/oauth/discord')
+async def oauth_discord(request:Request):
     params = dict(request.query_params)
     code = params.get('code')
     if not code:
@@ -106,8 +130,8 @@ async def discord_oauth(request:Request):
 
     return HTMLResponse(f'授權已完成，您現在可以關閉此頁面<br><br>Discord ID：{auth.user_id}')
 
-@app.route('/twitchauth',methods=['GET'])
-async def twitch_oauth(request:Request):
+@app.get('/oauth/twitch')
+async def oauth_twitch(request:Request):
     params = dict(request.query_params)
     code = params.get('code')
     if not code:
@@ -147,6 +171,14 @@ def handle_message(event:MessageEvent):
                 messages=[TextMessage(text=event.message.text)]
             )
         )
+
+@app.get("/to/discordauth")
+async def to_discordauth(request:Request):
+    return RedirectResponse(url=f"https://discord.com/api/oauth2/authorize?client_id={discord_oauth_settings['id']}&redirect_uri={discord_oauth_settings['redirect_uri']}&response_type=code&scope=identify%20connections")
+
+@app.get("/to/twitchauth")
+async def to_twitchauth(request:Request):
+    return RedirectResponse(url=f"https://id.twitch.tv/oauth2/authorize?client_id={twitch_oauth_settings['id']}&redirect_uri={twitch_oauth_settings['redirect_uri']}&response_type=code&scope=chat:read+channel:read:subscriptions+moderation:read+channel:read:redemptions+channel:manage:redemptions+channel:manage:raids+channel:read:vips+channel:bot+moderator:read:suspicious_users+channel:manage:polls+channel:manage:predictions&force_verify=true")
 
 # @app.get('/book/{book_id}',response_class=JSONResponse)
 # def get_book_by_id(book_id: int):
