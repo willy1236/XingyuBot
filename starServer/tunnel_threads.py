26c85a15e9d4d00b89edf5c7015d8371331653d8
willy1236 <66502303+willy1236@users.noreply.github.com>
2025-01-19 15:43:21 +0800

    fix: 增強子進程執行的錯誤處理，新增stdout和stderr的捕獲

--- starServer/tunnel_threads.py
+++ starServer/tunnel_threads.py
@@ -35,7 +35,7 @@ class ServeoThread(BaseThread):
         while not self._stop_event.is_set():
             log.debug(f"Starting {self.name} {reconnection_times}")
             #result = subprocess.run(["ssh", "-R", "cloudfoam:80:127.0.0.1:14000", "-R", "cloudfoamtwitch:80:127.0.0.1:14001", "serveo.net"], capture_output=True, text=True)
-            result = subprocess.run(["ssh", "-R", "yunmo:80:127.0.0.1:14000", "serveo.net"], capture_output=True, text=True)
+            result = subprocess.run(["ssh", "-R", "yunmo:80:127.0.0.1:14000", "serveo.net"], capture_output=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.debug(f'Stdout: {result.stdout}')
             log.debug(f'Stderr: {result.stderr}')
             log.debug(f'Exit status: {result.returncode}')
@@ -52,7 +52,7 @@ class LoopholeThread(BaseThread):
         reconnection_times = 0
         while not self._stop_event.is_set():
             log.debug(f"Starting {self.name} {reconnection_times}")
-            result = subprocess.run(["loophole", "http", "14000", "127.0.0.1", "--hostname", "cloudfoam"], capture_output=True, text=True)
+            result = subprocess.run(["loophole", "http", "14000", "127.0.0.1", "--hostname", "cloudfoam"], capture_output=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.debug(f'Stdout: {result.stdout}')
             log.debug(f'Stderr: {result.stderr}')
             log.debug(f'Exit status: {result.returncode}')
@@ -69,7 +69,7 @@ class LoopholeTwitchThread(BaseThread):
         reconnection_times = 0
         while not self._stop_event.is_set():
             log.info(f"Starting {self.name} {reconnection_times}")
-            result = subprocess.run(["loophole", "http", "14001", "127.0.0.1", "--hostname", "twitchcloudfoam"], capture_output=True, text=True)
+            result = subprocess.run(["loophole", "http", "14001", "127.0.0.1", "--hostname", "twitchcloudfoam"], capture_output=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.debug(f'Stdout: {result.stdout}')
             log.debug(f'Stderr: {result.stderr}')
             log.debug(f'Exit status: {result.returncode}')
@@ -87,7 +87,7 @@ class NgrokTwitchThread(BaseThread):
         callback_url = Jsondb.get_token('twitch_chatbot')['callback_uri'].split('://')[1]
         while not self._stop_event.is_set():
             log.info(f"Starting {self.name} {reconnection_times}")
-            result = subprocess.run(["ngrok", "http", f"--url={callback_url}", "14001"], capture_output=True, text=True)
+            result = subprocess.run(["ngrok", "http", f"--url={callback_url}", "14001"], capture_output=True, text=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             log.debug(f'Stdout: {result.stdout}')
             log.debug(f'Stderr: {result.stderr}')
             log.debug(f'Exit status: {result.returncode}')
