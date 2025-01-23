a29f70b1cfb898f613447652a316aa4f72d6f6c0
willy1236 <66502303+willy1236@users.noreply.github.com>
2025-01-23 14:27:29 +0800

    feat: 修正McssAPI類別中的HTTP請求參數，將params改為data以符合API要求

--- starlib/dataExtractor/others.py
+++ starlib/dataExtractor/others.py
@@ -28,10 +28,10 @@ class McssAPI:
             print(r.status_code)
 
     def excute_action(self, server_id: str, action: int):
-        params = {
+        data = {
             'action': action
         }
-        r = requests.post(f"{self.URL}/servers/{server_id}/execute/action", params=params, headers=self.headers)
+        r = requests.post(f"{self.URL}/servers/{server_id}/execute/action", data=data, headers=self.headers)
         r.raise_for_status()
         if r.ok:
             return r.text
@@ -39,10 +39,10 @@ class McssAPI:
             print(r.status_code)
     
     def excute_command(self, server_id: str, command: str):
-        params = {
+        data = {
             'command': command
         }
-        r = requests.post(f"{self.URL}/servers/{server_id}/execute/command", params=params, headers=self.headers)
+        r = requests.post(f"{self.URL}/servers/{server_id}/execute/command", data=data, headers=self.headers)
         r.raise_for_status()
         if r.ok:
             return r.text
