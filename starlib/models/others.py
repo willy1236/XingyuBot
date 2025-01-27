5206160e88782afafb882df2c3f57555e5c93379
willy1236 <66502303+willy1236@users.noreply.github.com>
2025-01-28 00:53:35 +0800

    feat: 將伺服器狀態類型改為使用McssServerStatues，並新增創建日期與記憶體分配欄位

--- starlib/models/others.py
+++ starlib/models/others.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel, Field
 from datetime import datetime
 from ..utils import BotEmbed
-
+from ..types.datatype import McssServerStatues
 mcss_server_status = {
     0: "離線",
     1: "啟動",
@@ -11,7 +11,7 @@ mcss_server_status = {
 class McssServer(BaseModel):
     """MCSS 伺服器設定模型"""
     server_id: str = Field(alias="serverId")
-    status: int
+    status: McssServerStatues
     name: str
     description: str
     path_to_folder: str = Field(alias="pathToFolder")
@@ -27,6 +27,8 @@ class McssServer(BaseModel):
     def embed(self):
         embed = BotEmbed.simple(self.name, self.description)
         embed.add_field(name="伺服器ID", value=self.server_id)
-        embed.add_field(name="伺服器類型", value=self.type)
-        embed.add_field(name="伺服器狀態", value=f"{self.status}（{mcss_server_status.get(self.status)}）")
+        embed.add_field(name="Minecraft類型", value=self.type)
+        embed.add_field(name="伺服器狀態", value=f"{self.status.value}（{str(self.status)}）")
+        embed.add_field(name="創建日期", value=self.creation_date.strftime("%Y-%m-%d %H:%M:%S"))
+        embed.add_field(name="記憶體分配", value=f"{self.java_allocated_memory} MB")
         return embed
