8bf3e2e1e678ec35afc6cee9021568f608324f28
willy1236 <66502303+willy1236@users.noreply.github.com>
2025-03-25 22:27:06 +0800

    feat: 更新MySQL引擎連接參數，新增charset設置以支持utf8mb4編碼

--- starlib/database/mysql.py
+++ starlib/database/mysql.py
@@ -40,7 +40,7 @@ class BaseSQLEngine:
         cache: dict[str, list[int]] | dict[NotifyChannelType, dict[int, tuple[int, Optional[int]]]] | dict[NotifyCommunityType, list[str]]
 
     def __init__(self,connection_url):
-        self.engine = create_engine(connection_url, echo=False, pool_pre_ping=True)
+        self.engine = create_engine(connection_url, echo=False, pool_pre_ping=True, connect_args={'charset': 'utf8mb4'})
         # SessionLocal = sessionmaker(bind=self.engine)
         # self.session:Session = SessionLocal()
         SQLModel.metadata.create_all(self.engine)
