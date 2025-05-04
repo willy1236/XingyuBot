08f9c46fa044f47529a3a12fe7699886fcfcb56f
willy1236 <66502303+willy1236@users.noreply.github.com>
2025-05-04 17:23:44 +0800

    fix: 增加動態語音頻道創建時的錯誤日誌記錄，改善權限檢查的可追蹤性

--- starDiscord/cmds/bot_event.py
+++ starDiscord/cmds/bot_event.py
@@ -302,7 +302,8 @@ class event(Cog_Extension):
                 }
                 try:
                     new_channel = await guild.create_voice_channel(name=f'{member.name}的頻道', reason='動態語音：新增',category=category,overwrites=overwrites)
-                except discord.errors.Forbidden:
+                except discord.errors.Forbidden as e:
+                    log.warning(e, exc_info=True)
                     await after.channel.send(f"{member.mention} 我無法創建動態語音頻道，請檢查我的權限", delete_after=5)
                     return
                 sclient.sqldb.add_dynamic_voice(new_channel.id,member.id,guild.id,None)
