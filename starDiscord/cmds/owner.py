722a3d008b4566ae2869a893fa4ef4974eed5f06
willy1236 <66502303+willy1236@users.noreply.github.com>
2025-06-10 17:52:31 +0800

    feat: 更新伺服器面板以顯示預設端口提示，當端口為25565時顯示可省略訊息

--- starDiscord/cmds/owner.py
+++ starDiscord/cmds/owner.py
@@ -195,6 +195,8 @@ class McServerPanel(discord.ui.View):
 				return
 
 			port = server.find_port() or "XXXXX（請確認這個數字是多少）"
+			if port == 25565:
+				port = "25565（預設端口可省略）"
 
 			await interaction.followup.send(f"伺服器IP位置：`{ip}:{port}`")
 		else:
