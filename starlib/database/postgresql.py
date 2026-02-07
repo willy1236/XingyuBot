7806877b96492d05c9ea4214f1187c1984e8a6b5
威立 <66502303+willy1236@users.noreply.github.com>
2026-02-07 19:50:45 +0800

    feat: 在 SQLRepository 初始化時新增語音時間計數器快取刷新邏輯

--- starlib/database/postgresql.py
+++ starlib/database/postgresql.py
@@ -1642,6 +1642,7 @@ class SQLRepository(
         self[DBCacheType.DynamicVoiceLobby] = self.get_all_dynamic_voice_lobby()
         self[DBCacheType.DynamicVoiceRoom] = self.get_all_dynamic_voice()
         self[DBCacheType.TwitchCmd] = self.get_raw_chat_command_all()
+        self.refresh_voice_time_counter_cache()
         log.debug("SQLDB: cache init.")
 
     def update_notify_channel(self, notify_type: NotifyChannelType):
