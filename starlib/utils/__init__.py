fa35b3dd6859f94570c9f8b18014dc187329a549
威立 <66502303+willy1236@users.noreply.github.com>
2026-03-29 18:35:51 +0800

    fix: 修正日誌記錄的參數名稱，統一為 format_str

--- starlib/utils/__init__.py
+++ starlib/utils/__init__.py
@@ -17,8 +17,8 @@ log_level = Jsondb.config.get("log_level",INFO)
 
 apsc_log = create_logger("apscheduler", WARNING, format_str="%(asctime)s [apsc/%(levelname)s] %(message)s")
 twitch_log = create_logger("twitchBot", DEBUG, format_str="%(asctime)s [twitchBot/%(levelname)s] %(message)s")
-web_log = create_logger("web", log_level, format="%(asctime)s [web/%(levelname)s] %(message)s")
-agent_log = create_logger("agent", log_level, format="%(asctime)s [agent/%(levelname)s] %(message)s")
+web_log = create_logger("web", log_level, format_str="%(asctime)s [web/%(levelname)s] %(message)s")
+agent_log = create_logger("agent", log_level, format_str="%(asctime)s [agent/%(levelname)s] %(message)s")
 
-# discord_log = create_logger("discord", WARNING, format="%(asctime)s [%(levelname)s] [discord] %(message)s")
-pywarnings = create_logger("py.warnings", ERROR, format="%(asctime)s [%(levelname)s] [py.warnings] %(message)s")
+# discord_log = create_logger("discord", WARNING, format_str="%(asctime)s [%(levelname)s] [discord] %(message)s")
+pywarnings = create_logger("py.warnings", ERROR, format_str="%(asctime)s [%(levelname)s] [py.warnings] %(message)s")
